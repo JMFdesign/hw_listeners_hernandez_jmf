fd31ad226eafa85dce93f9b8d0196837833409ed
JMF-Arnieri-Hernandez <jmfa89@ufl.edu>
2017-03-24 23:35:01 -0400

    edit main.js to suppress default action and log form fields

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 
-var clickSubmit = document.getElementsByClass('submit')[0];
+var clickSubmit = document.getElementById('submit_button');
+
+var firstName = document.getElementById('First_Name');
 
 clickSubmit.addEventListener('click', function (eventInfo) {
     eventInfo.preventDefault();
-    console.log(eventInfo);
-
-    var myElement = document.createElement('article');
-    myElement.innerHTML = '<p>This is click number x</p>';
-    document.getElementById('content').appendChild(myElement);
+    console.log('The firstName is: ' + 'firstName');
+    console.log('The lastName is: ' + 'Last_Name');
+    console.log('The email is: ' + 'email');
+    console.log('The message is: ' + 'message');
 });
