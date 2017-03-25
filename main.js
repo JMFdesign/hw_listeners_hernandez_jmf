9ca3fa1cfc268c32e165c27841303a31eb21d902
JMF-Arnieri-Hernandez <jmfa89@ufl.edu>
2017-03-24 21:51:13 -0400

    add JS that appends paragraph text to bottom of content

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 
 var clickMe = document.getElementById('clicker');
+
+clickMe.addEventListener('click', function () {
+    var myElement = document.createElement('article');
+    myElement.innerHTML = '<p>This is click number x</p>';
+    document.getElementById('content').appendChild(myElement);
+});
