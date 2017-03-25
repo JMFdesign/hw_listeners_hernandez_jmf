18ad3415cac308d13e3f1356febac1f5340238d2
JMF-Arnieri-Hernandez <jmfa89@ufl.edu>
2017-03-25 01:52:31 -0400

    add JavaScript for third custom event listener

--- main.js
+++ main.js
@@ -14,3 +14,11 @@ secretMessage.addEventListener('click', function(eventInfo) {
     eventInfo.preventDefault();
     console.log('Just keep swimming, just keep swimming... Swimming, swimming, swimming...');
 });
+
+// Listening for Double Click - Change font and size of entry <p> when event occurs
+
+var colorChange1 = document.getElementsByTagName('p')[1];
+
+colorChange.addEventListener('dblclick', function() {
+    colorChange.className = 'changing';
+});
