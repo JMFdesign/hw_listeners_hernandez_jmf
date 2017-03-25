f8094e1c8cbe8a40c8e7d6beeb26da11a183f688
JMF-Arnieri-Hernandez <jmfa89@ufl.edu>
2017-03-25 01:35:12 -0400

    add JavaScript for second custom event listener

--- main.js
+++ main.js
@@ -6,4 +6,11 @@ growTitle.addEventListener('mouseover', function() {
     growTitle.className = 'growing';
 });
 
-//
+//Listening for Click - Log Secret Message when "Read More" is clicked
+
+var secretMessage = document.getElementsByClassName('links')[0].children[0];
+
+secretMessage.addEventListener('click', function(eventInfo) {
+    eventInfo.preventDefault();
+    console.log('Just keep swimming, just keep swimming... Swimming, swimming, swimming...')
+});
