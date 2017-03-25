98f3cfd5874ae8c0af6eecfa1228340ac92a33ce
JMF-Arnieri-Hernandez <jmfa89@ufl.edu>
2017-03-25 00:28:37 -0400

    edit main.js to include form information in the console

--- main.js
+++ main.js
@@ -2,11 +2,14 @@
 var clickSubmit = document.getElementById('submit_button');
 
 var firstName = document.getElementById('First_Name');
+var lastName = document.getElementById('Last_Name');
+var emailAddress = document.getElementById('Email');
+var theMessage = document.getElementById('Message');
 
 clickSubmit.addEventListener('click', function (eventInfo) {
     eventInfo.preventDefault();
-    console.log('The firstName is: ' + 'firstName');
-    console.log('The lastName is: ' + 'Last_Name');
-    console.log('The email is: ' + 'email');
-    console.log('The message is: ' + 'message');
+    console.log('The firstName is: ' + firstName.value);
+    console.log('The lastName is: ' + lastName.value);
+    console.log('The email is: ' + emailAddress.value);
+    console.log('The message is: ' + theMessage.value);
 });
