aa92b3d29edc1b40f16e59c0e866467c73f0c975
JMF-Arnieri-Hernandez <jmfa89@ufl.edu>
2017-03-25 00:17:23 -0400

    add click count to the click listener

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 
-var clickMe = document.getElementById('clicker');
+var clickMe = document.getElementById('clicker'), count = 0;
 
 clickMe.addEventListener('click', function () {
-    var myElement = document.createElement('article');
-    myElement.innerHTML = '<p>This is click number x</p>';
+    count += 1;
+    var myElement = document.createElement('p');
+    myElement.innerHTML = 'This is click number ' + count;
     document.getElementById('content').appendChild(myElement);
 });
